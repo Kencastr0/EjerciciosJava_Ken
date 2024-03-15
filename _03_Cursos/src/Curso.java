bb3c02b2f97406a5cab1bb00479f89f1021ae14f
Ken Castro <kacp.castro08@gmail.com>
2024-03-15 13:12:21 -0500

    Correccion de menu

--- _03_Cursos/src/Curso.java
+++ _03_Cursos/src/Curso.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Curso {
     private String codigo;
     private String nombreC;
     private ArrayList<Estudiante> listaEstudiantes;
     private static int idEstudiante = 1;
+
     public Curso(String codigo, String nombreC) {
         this.codigo = codigo;
         this.nombreC = nombreC;
         this.listaEstudiantes = new ArrayList<>();
     }
-    //METODOS
+
+    //Métodos
     public void guardarEstudiante(Scanner objScan) {
         System.out.println("Agregar nuevo estudiante");
         System.out.println("Ingresa el nombre del estudiante: ");
@@ -25,8 +28,8 @@ public class Curso {
             System.out.println("No se pudo agregar al estudiante!");
         }
     }
+
     public void listarEstudiantes() {
-        //validar si la lista está vacía
         if (this.listaEstudiantes.isEmpty()) {
             System.out.println("El curso ".concat(this.nombreC).concat(" no tiene estudiantes registrados"));
         } else {
@@ -36,6 +39,7 @@ public class Curso {
             }
         }
     }
+
     public void eliminarEstudiante(Scanner objScan) {
         listarEstudiantes();
         System.out.println("Ingrese le ID del estudiante que desea eliminar del curso: ");
@@ -46,24 +50,31 @@ public class Curso {
             System.out.println("¡No se puedo eliminar el estudiante!");
         }
     }
+
     public String getCodigo() {
         return codigo;
     }
+
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
+
     public String getNombreC() {
         return nombreC;
     }
+
     public void setNombreC(String nombreC) {
         this.nombreC = nombreC;
     }
+
     public ArrayList<Estudiante> getListaEstudiantes() {
         return listaEstudiantes;
     }
+
     public void setListaEstudiantes(ArrayList<Estudiante> listaEstudiantes) {
         this.listaEstudiantes = listaEstudiantes;
     }
+
     @Override
     public String toString() {
         return "Curso\n" +
