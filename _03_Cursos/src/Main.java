be5b86efdfcdad5aff40011820a38f4b660ad446
Ken Castro <kacp.castro08@gmail.com>
2024-03-15 13:11:06 -0500

    Correccion de menu

--- _03_Cursos/src/Main.java
+++ _03_Cursos/src/Main.java
@@ -2,91 +2,127 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Instancias
+
         Scanner objScan = new Scanner(System.in);
         GestionCurso objGestion = new GestionCurso();
+
         int option = 0;
+
         do {
             System.out.println("""
-                    MENU DE OPCIONES
+                                        
+                    MENÚ DE OPCIONES:
+                                        
                     1. Administrar cursos.
                     2. Administrar estudiantes.
                     3. Salir.
+                                        
                     """);
             option = objScan.nextInt();
             switch (option) {
                 case 1:
+
                     int option2 = 0;
+
                     do {
                         System.out.println("""
-                                MENU DE CURSOS
+                                                                
+                                MENÚ DE CURSOS:
+                                                                
                                 1. Crear un curso.
                                 2. Buscar curso por código.
                                 3. Listar cursos
                                 4. Salir
+                                                                
                                 """);
                         option2 = objScan.nextInt();
                         switch (option2) {
+
                             case 1:
+
                                 objGestion.guardarCurso(objScan);
                                 break;
+
                             case 2:
+
                                 objGestion.listarCursos();
-                                System.out.println("Ingresa el código del curso: ");
+                                System.out.println("Código del curso: ");
                                 String codigo = objScan.next();
-                                System.out.println(objGestion.buscarPorCodigo(codigo).toString());
-                                break;
+                                if (objGestion.buscarPorCodigo(codigo) == null) {
+                                    System.out.println("Curso no encontrado");
+                                    break;
+                                }else {
+                                    System.out.println(objGestion.buscarPorCodigo(codigo).toString());
+                                    break;
+                                }
+
                             case 3:
+
                                 objGestion.listarCursos();
                                 break;
                         }
                     } while (option2 != 4);
                     break;
+
                 case 2:
+
                     int option3 = 0;
+
                     do {
                         System.out.println("""
-                                Menú gestion Estudiantes
+                                                                
+                                MENÚ GESTIÓN DE ESTUDIANTES:
+                                                                
                                 1. Agregar Estudiante.
                                 2. Eliminar un Estudiante.
                                 3. listar Estudiantes.
                                 4. Salir
+                                                                
                                 """);
                         option3 = objScan.nextInt();
+
                         switch (option3) {
+
                             case 1:
+
                                 objGestion.listarCursos();
-                                System.out.println("Ingrese el código del curso donde desea inscribir el estudiante");
+                                System.out.println("Código del curso donde desea inscribir el estudiante");
                                 String codigo = objScan.next();
                                 Curso curso = objGestion.buscarPorCodigo(codigo);
+
                                 if (curso == null) {
-                                    System.out.println("Curso no encontrado");
+                                    System.out.println("Class not found");
                                 } else {
                                     curso.guardarEstudiante(objScan);
                                 }
                                 break;
-                            case 2: //caso para eliminar un estudiante
+
+                            case 2: //Caso para eliminar un estudiante
                                 //1. listar cursos
                                 objGestion.listarCursos();
-                                //2. preguntar al usuario cúal es el curso del estudiante a eliminar
-                                System.out.println("Ingrese el código del curso del estudiante a eliminar");
+                                //2. Curso del estudiante a eliminar
+                                System.out.println("Código del curso del estudiante a eliminar");
                                 codigo = objScan.next();
-                                //3. llamar al método que elimina
+                                //3. Llamar al método eliminar
                                 Curso objCurso = objGestion.buscarPorCodigo(codigo);
-                                if (objCurso==null){
-                                    System.out.println("Código no valido!");
-                                }else {
-                                    //eliminarq
+
+                                if (objCurso == null) {
+                                    System.out.println("Code invalid");
+                                } else {
+                                    //Eliminar
                                     objCurso.eliminarEstudiante(objScan);
                                 }
                                 break;
+
                             case 3:
+
                                 objGestion.listarCursos();
-                                System.out.println("Ingrese el código del curso para ver lista de estudiantes");
+                                System.out.println("Código del curso para ver lista de estudiantes");
                                 codigo = objScan.next();
                                 curso = objGestion.buscarPorCodigo(codigo);
+
                                 if (curso == null) {
-                                    System.out.println("Curso no encontrado");
+                                    System.out.println("Class not found");
                                 } else {
                                     curso.listarEstudiantes();
                                 }
